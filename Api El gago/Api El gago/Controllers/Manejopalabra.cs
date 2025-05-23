062bca4ddf91ed2a7a0743a228a437dcd3d4d439
KEWIN ANDRES BECERRA FERNANDEZ <kewinbecerra1113588@correo.itm.edu.co>
2025-05-22 21:52:58 -0500

    se inserto manejo de palabras y palabras

--- Api El gago/Api El gago/Controllers/Manejopalabra.cs
+++ Api El gago/Api El gago/Controllers/Manejopalabra.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace Api_El_gago.Controllers
 {
     public class Manejopalabra
     {
+        public static bool manejoporcodigo(palabras palabra ) {
+
+            SqlConnection conector = Models.DB.conectar("");
+        }
+
     }
 }
