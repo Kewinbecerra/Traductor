bb5120f00e8b39fa8b607d490910088e146a6b2f
KEWIN ANDRES BECERRA FERNANDEZ <kewinbecerra1113588@correo.itm.edu.co>
2025-05-23 21:47:30 -0500

    Se crearon los controllers de get

--- Api El gago/Api El gago/Controllers/Manejopalabra.cs
+++ Api El gago/Api El gago/Controllers/Manejopalabra.cs
@@ -4,19 +4,83 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using Conector;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Net.Http;
+using System.Net;
+using Microsoft.Ajax.Utilities;
 namespace Api_El_gago.Controllers
 {
-    public class Manejopalabra
+    public class Manejopalabra : ApiController
     {
-       /* public static bool manejoporcodigo(Palabras palabra ) {
-            
-            SqlConnection conector = Models.DB.conectar("idiomas");
+        [RoutePrefix("api/palabras")]
+        public class PalabrasController : ApiController
+        {
+            // GET api/palabras/sinonimos/5
+            [HttpGet]
+            [Route("sinonimos/{codigoIngles}")]
+            public IHttpActionResult GetSinonimos(int codigoIngles)
+            {
+                using (idiomasEntities db = new idiomasEntities())
+                {
+                    var sinonimos = db.español
+                                      .Where(p => p.codigoingles == codigoIngles)
+                                      .ToList();
+
+                    if (!sinonimos.Any())
+                    {
+                        return NotFound();
+                    }
+
+                    return Ok(sinonimos);
+                }
+            }
+
+            // GET api/palabras/traduccion/perro
+            [HttpGet]
+            [Route("traduccion/{palabra}")]
+            public IHttpActionResult GetTraduccion(string palabra)
+            {
+                using (idiomasEntities db = new idiomasEntities())
+                {
+                    var palabra_espanol = db.español.FirstOrDefault(x => x.Palabra == palabra);
+
+                    if (palabra_espanol == null)
+                    {
+                        return NotFound();
+                    }
+
+                    var palabra_ingles = db.Ingles.FirstOrDefault(x => x.codigo == palabra_espanol.codigoingles);
+
+                    if (palabra_ingles == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return Ok(palabra_ingles);
+                }
+            }
+        }
+
+        private HttpResponseMessage opercion([FromBody] Palabras objCuenta, EntityState operacion)
+        {
+            int resp = 0;
+            HttpResponseMessage objMenRespuesta = null;
             try
             {
-                string sql = "select * from spa";
+                using (idiomasEntities objEntidad = new idiomasEntities())
+                {
+                    objEntidad.Entry(objCuenta).State = operacion;
+                    resp = objEntidad.SaveChanges();
+                    objMenRespuesta = Request.CreateResponse(HttpStatusCode.OK, resp);
+                }
             }
-            catch { }
-        }*/
+            catch (Exception er)
+            {
+                objMenRespuesta = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, er.Message);
+            }
+            return objMenRespuesta;
+        }
 
     }
 }
