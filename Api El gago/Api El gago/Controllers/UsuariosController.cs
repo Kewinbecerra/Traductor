2473f4239e264c6888b87159f88a7476a3546e17
KEWIN ANDRES BECERRA FERNANDEZ <kewinbecerra1113588@correo.itm.edu.co>
2025-05-26 12:17:04 -0500

    Se inserto la base de datos de usuarios y se creo el controller para el manejo de esta

---
+++ Api El gago/Api El gago/Controllers/UsuariosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Conector;
+
+namespace Api_El_gago.Controllers
+{
+    [RoutePrefix("api/usuarios")]
+    public class UsuariosController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Usuarios_roles> GetUsuario()
+        {
+            using (UsuariosEntities usuarios = new UsuariosEntities()) {
+             
+             return usuarios.Usuarios_roles.ToList();
+            }
+        }
+    }
+}
